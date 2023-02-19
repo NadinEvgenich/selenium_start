3254cae95bf90cde322e764ecb240dc5e00b4763
NadinEvgenich <70627193+NadinEvgenich@users.noreply.github.com>
2023-02-19 17:53:37 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,11 +6,23 @@ import logging
 from selenium import webdriver
 
 
+logging.basicConfig(filename="selenium.log",
+                    format='%(asctime)s:%(levelname)s:%(name)s - %(message)s',
+                    encoding='utf-8',
+                    datefmt='%m/%d/%Y %I:%M:%S',
+                    level=logging.INFO
+                    )
+
+
+class Listener(AbstractEventListener):
+    logger = logging.getLogger('ListenerLoger')
+
+
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome", choices=["chrome", "firefox", "opera", "safari", "MicrosoftEdge"])
     parser.addoption("--executor", default="localhost")
     parser.addoption("--vnc", action="store_true", default=False)
-    parser.addoption("--log_level", action="store", default="DEBUG")
+    parser.addoption("--logs", default=True)
     parser.addoption("--bversion", action="store", default="102.0")
 
 
@@ -21,14 +33,9 @@ def driver(request):
     log_level = request.config.getoption("--log_level")
     version = request.config.getoption("--bversion")
     vnc = request.config.getoption("--vnc")
+    logs = request.config.getoption("--logs")
+    test_name = request.node.name
 
-    logger = logging.getLogger(request.node.name)
-    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
-    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    logger.addHandler(file_handler)
-    logger.setLevel(level=log_level)
-
-    logger.info(f"===> Test {request.node.name} started at {time.asctime()}")
 
     if executor == "localhost":
         if browser == "chrome":
@@ -47,21 +54,19 @@ def driver(request):
 
         driver = webdriver.Remote(
             desired_capabilities=capabilities,
-            command_executor=executor_url
-        )
+            command_executor=executor_url)
+        , Listener())
+  
 
-    driver.log_level = log_level
-    driver.logger = logger
-    driver.test_name = request.node.name
-
-    logger.info("Browser:{}".format(browser))
+    logger.info("Test {} started with {} {}".format(test_name, browser, version))
 
     driver.maximize_window()
     driver.implicitly_wait(5)
 
     def fin():
         driver.quit()
-        logger.info(f"===> Test {request.node.name} finished at {time.asctime()}")
+         logger.info("Browser {} closed".format(browser))
+         logger.info("Test {} finished".format(test_name))
 
     request.addfinalizer(fin)
     return driver
