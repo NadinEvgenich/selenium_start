c20684fcf624890ffa064bb32d910580674b27fc
NadinEvgenich <70627193+NadinEvgenich@users.noreply.github.com>
2023-02-21 17:15:34 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -23,6 +23,8 @@ class Listener(AbstractEventListener):
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome", choices=["chrome", "firefox", "opera", "safari", "MicrosoftEdge"])
     parser.addoption("--headless", action="store_true")
+    parser.addoption("--disable-dev-shm-usage", action="store_true")
+    parser.addoption("--window-size", default="1920x1080")
     parser.addoption("--executor", default="localhost")
     parser.addoption("--url", action="store", default="https://demo.opencart.com")
     parser.addoption("--vnc", action="store_true", default=False)
@@ -34,6 +36,8 @@ def pytest_addoption(parser):
 def driver(request):
     browser = request.config.getoption("--browser")
     headless = request.config.getoption("--headless")
+    disable_dev_shm_usage = request.config.getoption("--disable-dev-shm-usage")
+    window_size = request.config.getoption("--window-size")
     executor = request.config.getoption('--executor')
     logs = request.config.getoption("--logs")
     version = request.config.getoption("--bversion")
@@ -46,6 +50,8 @@ def driver(request):
         if browser == "chrome":
           options = ChromeOptions()
           options.headless = headless
+          options.disable_dev_shm_usage = disable_dev_shm_usage
+          options.window_size = window_size
           driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options) 
     else:
         executor_url = f"http://{executor}:4444/wd/hub"
