36ee6fb5aaf718c940996d5f6f85174be128cdee
NadinEvgenich <70627193+NadinEvgenich@users.noreply.github.com>
2023-02-21 12:23:31 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 import logging
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 
 
@@ -21,6 +21,9 @@ class Listener(AbstractEventListener):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome", choices=["chrome", "firefox", "opera", "safari", "MicrosoftEdge"])
+    parser.addoption(
+        "--driver_folder", default=os.path.expanduser("~/Otus_course/selenium_start/drivers")
+    )
     parser.addoption("--executor", default="localhost")
     parser.addoption("--url", action="store", default="https://demo.opencart.com")
     parser.addoption("--vnc", action="store_true", default=False)
@@ -31,6 +34,7 @@ def pytest_addoption(parser):
 @pytest.fixture()
 def driver(request):
     browser = request.config.getoption("--browser")
+    driver_folder = request.config.getoption("--driver_folder")
     executor = request.config.getoption('--executor')
     logs = request.config.getoption("--logs")
     version = request.config.getoption("--bversion")
@@ -41,7 +45,11 @@ def driver(request):
 
     if executor == "localhost":
         if browser == "chrome":
-            driver = webdriver.Chrome()
+            options = ChromeOptions()
+            options.headless = headless
+            driver = webdriver.Chrome(
+              options=options, executable_path=f"{driver_folder}{os.sep}chromedriver"
+        )
     else:
         executor_url = f"http://{executor}:4444/wd/hub"
         capabilities = {
